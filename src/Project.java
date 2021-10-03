6fa9a6a70fff8a4b9ea63ea45b81d01cb34aa10a
Eepakura <alena.pozdeeva1@mail.ru>
2021-10-03 18:57:15 +0500

    Добавлены классы работы с консолью и хранилище вопросов

--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,14 @@
-public class Project {
-    public static void main(String[] args){
-        System.out.println("12345");
+public class Project
+{
+    public static void main(String[] args)
+    {
+        Response responser = new Response();
+        Data_input reader = new Data_input();
+        responser.sayHello();
+        while (true)
+        {
+            String str_input = reader.read();
+            responser.response(str_input);
+        }
     }
 }
